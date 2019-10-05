8fc6af8290bafea53c3196883e60674a3fee35af
youngjun.jin <yzzzzun@gmail.com>
2019-10-05 13:25:44 +0900

    Controlled component 를 UnControlled component로 변경

--- src/AddPlayerForm.js
+++ src/AddPlayerForm.js
@@ -2,33 +2,18 @@ import React, {Component} from 'react';
 
 export class AddPlayerForm extends Component {
 
-	state = {
-		value: ''
-	}
-
-	handleValueChange = (e) => {
-		this.setState({
-			value : e.target.value
-		});
-	}
+	textInput = React.createRef();
 
 	handleSubmit = (e) => {
 		e.preventDefault();
-
-		const form = document.getElementById("form");
-		const player = document.getElementById("player");
-
-		console.log(form.checkValidity());
-		console.log(player.validity.valid);
-
-		this.props.addPlayer(this.state.value);
-		this.setState({value: ''});
+		this.props.addPlayer(this.textInput.current.value);
+		e.currentTarget.reset();
 	}
 
 	render() {
 		return (
 			<form className="form" id="form" onSubmit={this.handleSubmit} noValidate>
-				<input className="input" id="player" type="text" placeholder="enter a player's name" value={this.state.value} onChange={this.handleValueChange} required></input>
+				<input className="input" id="player" ref={this.textInput} type="text" placeholder="enter a player's name" required></input>
 				<input className="input" type="submit" value="Add Player"></input>
 			</form>
 		);
