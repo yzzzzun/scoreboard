e44a2a5683f25069e0c16629f07436e97fa2383f
youngjun.jin <yzzzzun@gmail.com>
2019-10-05 13:09:21 +0900

    ShouldComponentUpdate 로 렌더링 성능 개선

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,11 @@
 import React from "react";
 import {Counter} from "./Counter";
 
-export class Player extends React.PureComponent {
+export class Player extends React.Component {
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		return nextProps.score !== this.props.score ? true : false
+	}
 
 	render() {
 		console.log(this.props.name, ' rendered');
